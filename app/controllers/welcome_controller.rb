52bf32fdf47180042c13774f0808e392027f5ec3
Clint <clintonjamesbaker@gmail.com>
2017-02-23 20:35:18 +0000

    Late initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,17 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  
+  def about
+  end
+  
+  def services
+    
+  end
+  
+  def portfolio
+  end
+  
+  def contact
+  end
+end
